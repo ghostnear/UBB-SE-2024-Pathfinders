c6065a2c0d5fb9455fde32b13732cd53e5b27d27
914LauranDavid <115073641+914LauranDavid@users.noreply.github.com>
2024-04-04 21:35:24 +0300

    Update structure (#2)
    * use wfp
    
    * change domain to model

---
+++ board-games/board-games/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace board_games
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
