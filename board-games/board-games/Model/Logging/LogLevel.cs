fe4afee1193c06f677564b134e9a807c6366959e
914LauranDavid <115073641+914LauranDavid@users.noreply.github.com>
2024-04-08 21:21:00 +0300

    simple logger (#3)
    * simple logger
    
    * specifications

---
+++ board-games/board-games/Model/Logging/LogLevel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace board_games.Model.Logging
+{
+    /// <summary>
+    /// Used to measure the importance of a log message. Info is the least important, while Error is the most important.
+    /// </summary>
+    public enum LogLevel
+    {
+        Info,
+        Warn,
+        Error
+    }
+}
