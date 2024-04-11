216fb392de4bb2a1e28599c8abbdf5a1eb1fefdc
RLSMname <115074855+RLSMname@users.noreply.github.com>
2024-04-12 02:50:50 +0300

    Some windows

---
+++ board-games/board-games/View/SkillIssueBro/Menus/SkillIssueBroNumberPlayers.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace board_games.View.SkillIssueBro.Menus
+{
+    /// <summary>
+    /// Interaction logic for SkillIssueBroNumberPlayers.xaml
+    /// </summary>
+    public partial class SkillIssueBroNumberPlayers : UserControl
+    {
+        public SkillIssueBroNumberPlayers()
+        {
+            InitializeComponent();
+        }
+    }
+}
